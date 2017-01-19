0641cb79cd16e42092c338da0e45ea526a8024eb
George <George>
2017-01-18 22:43:34 -0500

    add shifting

--- interview-questions/bitwise.js
+++ interview-questions/bitwise.js
@@ -4,3 +4,11 @@ function isOdd(num) {
     else
         return false;
 }
+
+function shift(num, shift = 3) {
+    var rotate_bits = num & 0xe0000000;
+    num = num << shift;
+    rotate_bits = rotate_bits >>> (32 - shift);
+    num = num | rotate_bits;
+    return num;
+}
